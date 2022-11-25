f432b3daae5be3a90f479423544fbf9a79b2695f
nacil420 <ramdhanihw420@gmail.com>
2022-11-25 13:39:24 +0700

    update2

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "187756ef911dfca0"; }
+/******/ 		__webpack_require__.h = function() { return "743270a05526e042"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,6 +6,7 @@ import {AiOutlineClose,AiOutlineMail,AiOutlineMenu} from 'react-icons/ai'
 import {FaGithub, FaInstagramSquare} from 'react-icons/fa'
 import {BsWhatsapp} from 'react-icons/bs'
 import logoImg from '../public/assets/navLogo.png'
+import navLogo from '../public/assets/navLogo.png'
 
 const Navbar = () => {
     const [nav, setNav] = useState(false)
@@ -56,7 +57,7 @@ const Navbar = () => {
             <div className={nav ? 'fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500' : "fixed left-[-100%] top-0 p-10 ease-in duration-500"}>
             <div>
                 <div className='flex w-full items-center justify-between'>
-                    <Image src='/../public/assets/navLogo.png' width='65' height='35' alt='/' />
+                    <Image src={navLogo} alt='/' width='65' height='35'  />
                 <div onClick={handleNav} className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer'>
                     <AiOutlineClose />
                 </div>
